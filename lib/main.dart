a668a673b29d13e593e6f391fc84c7d019cd60ce
Henrique Dezani <dezani@gmail.com>
2021-03-02 18:30:23 -0300

    Versão inicial com calculo do IMC

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:imc_app/ui/imc.page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  var b = new Botao(onClick: () {});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primaryColor: Colors.blue,
       ),
-      home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: Text('IMC'),
-          centerTitle: false,
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Flexible(
-              flex: 1, // 1/3
-              child: Container(
-                margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Peso",
-                        hintText: "Peso em kg.",
-                      ),
-                      keyboardType: TextInputType.number,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Altura",
-                        hintText: "Altura em metros",
-                      ),
-                      keyboardType: TextInputType.numberWithOptions(
-                        decimal: true,
-                        signed: false,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      width: double.infinity,
-                      child: RaisedButton(
-                        color: Theme.of(context).primaryColor,
-                        child: Text(
-                          "Calcular IMC",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 2, // 2/3
-              child: Center(
-                child: Image.asset('assets/images/ideal.png'),
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: ImcPage(),
     );
   }
-}
-
-class Botao {
-  Function onClick;
-  Botao({this.onClick});
 }
